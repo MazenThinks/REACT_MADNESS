9b128a5fc3d60ffcc7cb5525a0222bf6f55ce694
MazenThinks <mazenmagdy100.mm@gmail.com>
2023-11-29 16:24:57 +0200

    applying bootstrap to the list if items exist

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -3,7 +3,7 @@ import { Fragment } from "react";
 function ListGroup() {
   let items = ["New York", "San Francisco", "London", "Paris", "Tokyo"];
 
-  items = [];
+  //items = [];
 
   const Zeroitems = () => {
     return items.length === 0 ? <p>No item found</p> : null;
@@ -17,7 +17,7 @@ function ListGroup() {
       ) : (
         <ul className="list-group">
           {items.map((item) => (
-            <li key={item}>{item}</li>
+            <li className ="list-group-item" key={item}>{item}</li>
           ))}
         </ul>
       )}

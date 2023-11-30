cb74fa5e0c5221b71e9ae36d96207508bff8e2dc
MazenThinks <mazenmagdy100.mm@gmail.com>
2023-11-30 14:08:05 +0200

    using Props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import Message from "./Message";
 import ListGroup from "./Components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "London", "Paris", "Tokyo"];
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,8 +1,11 @@
 import { Fragment, useState } from "react";
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "London", "Paris", "Tokyo"];
+interface Props {
+  items: string[];
+  heading: string;
+}
 
+function ListGroup({ items, heading }: Props) {
   // hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
@@ -10,7 +13,7 @@ function ListGroup() {
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 ? (
         <p>No item found</p>
       ) : (
